0ba600d31e0f76cf5cdc586850c9af8d4841c369
vadyms <vadym1981@gmail.com>
2015-01-08 13:09:20 +0200

    AddedProject
    VS MVC project

---
+++ Mvc3Application2/Mvc3Application2/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Mvc3Application2.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //if (model.Username == "Jed" && model.Password == "albao") //Simulate data store
+                if (model.Username == "a" && model.Password == "a") //Simulate data store
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    return RedirectToAction("index", "home");
+                }
+                {
+                    ModelState.AddModelError("", "Invalid username or password");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Mvc3Application2/Mvc3Application2/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc3Application2.Controllers
+{
+    public class LoginModel
+    {
+        [Required]
+        [StringLength(4)]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(4)]
+        public string Password { get; set; }
+    }
+}
