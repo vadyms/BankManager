6ab12d8a71c164a0c33b679fd67cb40c2ae0b677
vadyms <vadym1981@gmail.com>
2015-01-08 18:38:17 +0200

    Added New Code
    Bank Manager

---
+++ BankManager/BankManager/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankManager.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LogIn(BankManager.Models.UserModel user)
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Registration(BankManager.Models.UserModel user )
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BankManager/BankManager/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BankManager.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [StringLength(150)]
+        public string Login { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20,MinimumLength=6)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public bool RememberMe { get; set; }
+    }
+}
