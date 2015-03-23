4d213f9d785e81e7c181bf116c1be5493bf75d09
vadyms <vadym1981@gmail.com>
2015-03-23 13:22:33 +0200

    Added SQL scripts to create DB

---
+++ BankManager/DBBankManager/ClientStatuses.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[ClientStatuses]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [StatusName] NCHAR(10) NOT NULL
+)
---
+++ BankManager/DBBankManager/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Login] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL, 
+    [PasswordSalt] NVARCHAR(MAX) NOT NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [Address] NCHAR(10) NOT NULL
+)
