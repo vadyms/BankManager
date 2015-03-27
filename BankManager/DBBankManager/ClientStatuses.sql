f39717e47ceeaea2e4f3f2b2211a614db7040095
vadyms <vadym1981@gmail.com>
2015-03-27 10:47:22 +0200

    Removed unused folders
    Removed unused folders

--- BankManager/DBBankManager/ClientStatuses.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[ClientStatuses]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [StatusName] NCHAR(10) NOT NULL
-)
--- BankManager/DBBankManager/Users.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Users]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Login] NVARCHAR(50) NOT NULL, 
-    [Password] NVARCHAR(50) NOT NULL, 
-    [PasswordSalt] NVARCHAR(MAX) NOT NULL, 
-    [Email] NVARCHAR(50) NULL, 
-    [Address] NCHAR(10) NOT NULL
-)
