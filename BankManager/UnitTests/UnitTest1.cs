cf540b31bd188dcf579420e2842d30e1a8171d2c
vadyms <vadym1981@gmail.com>
2015-04-09 17:44:35 +0300

    Updated unit test
    Updated unit test

---
+++ BankManager/UnitTests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Interactions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //IWebDriver driver = new InternetExplorerDriver(@"C:\WebDriverSrv");
+            IWebDriver driver = new InternetExplorerDriver();
+            string sUrl = "http://www.nice.com";
+            //sUrl = "http://www.nice.com/enterprise-solutions";
+
+            driver.Navigate().GoToUrl(sUrl);
+            System.Threading.Thread.Sleep(10000);
+
+            Actions act = new Actions(driver);
+            IWebElement menu = driver.FindElement(By.ClassName("nav-link"));
+            act.MoveToElement(menu).Build().Perform();
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("document.getElementById('dd-our-offering').style.overflow='visible'");
+            driver.FindElement(By.LinkText("Solutions")).Click();
+            driver.FindElement(By.LinkText("Voice of the Customer")).Click();
+            System.Threading.Thread.Sleep(6000);
+            driver.FindElement(By.LinkText("Customer Journey Optimization")).Click();
+            System.Threading.Thread.Sleep(6000);
+            driver.FindElement(By.CssSelector("a[class='thickbox initThickbox-processed']")).Click();
+            //ClientController clientCrtl = new ClientController(_clientService,_statusService);
+            //Assert.AreNotEqual(clientCrtl, null);
+        }
+    }
+}
--- BankManager/UnitTests/UnitTestClients.cs
+++ BankManager/UnitTests/UnitTestClients.cs
@@ -11,6 +11,8 @@ using BankManager.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
+using BankManager.Abstract;
+using BankManager.BankData;
 
 namespace UnitTests
 {
@@ -20,6 +22,7 @@ namespace UnitTests
         private readonly IService<Client> _clientService = null;
         private readonly IService<ClientStatus> _statusService = null;
         private readonly IRepository<Client> _iClientRepository;
+        private readonly IService<User> _userService = null;
 
         [TestMethod]
         public void CreateTestMethod()
@@ -41,13 +44,23 @@ namespace UnitTests
         [TestMethod]
         public void ClientControllerTest()
         {
-            //IWebDriver driver = new InternetExplorerDriver(@"C:\WebDriverSrv");
-            IWebDriver driver = new InternetExplorerDriver();
-            string sUrl = "http://google.com";
-            driver.Navigate().GoToUrl(sUrl);
+            Mock<IService<User>> mock = new Mock<IService<User>>();
+            mock.Setup(m => m.FindAll()).Returns(new List<User> {
+                new User { Id=1, Login="1", Password="1", PasswordSalt="1", Address="1", Email="1@1.com" },
+                new User { Id=2, Login="2", Password="2", PasswordSalt="2", Address="2", Email="2@2.com" },
+                new User { Id=3, Login="3", Password="3", PasswordSalt="3", Address="3", Email="3@3.com" }
+            }.AsQueryable());
+
+            UserController controller = new UserController(mock.Object);
+            var result = controller.Index(); ;
+
             //ClientController clientCrtl = new ClientController(_clientService,_statusService);
             //Assert.AreNotEqual(clientCrtl, null);
 
+            //IWebDriver driver = new InternetExplorerDriver(@"C:\WebDriverSrv");
+            //IWebDriver driver = new InternetExplorerDriver();
+            //string sUrl = "http://google.com";
+            //driver.Navigate().GoToUrl(sUrl);
         }
         [TestMethod]
         public void ClientServiceTestMethod()
