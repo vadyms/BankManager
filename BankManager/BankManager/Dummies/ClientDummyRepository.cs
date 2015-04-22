c177c39f50f0ebc8b93bff7726ce49a87984c7fc
vadyms <vadym1981@gmail.com>
2015-04-22 17:36:54 +0300

    Added dummies for testing

---
+++ BankManager/BankManager/Dummies/ClientDummyRepository.cs
@@ -0,0 +1,31 @@
+﻿using BankManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankManager.Dummies
+{
+    public class ClientDummyRepository : IRepository<User>
+    {
+        public int Create(User t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(User t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
