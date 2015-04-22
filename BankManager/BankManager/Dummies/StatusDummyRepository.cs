80ff02eb7e5a59b4d6cfca862920bbbd1f8eb430
vadyms <vadym1981@gmail.com>
2015-04-22 17:37:08 +0300

    Added dummies for testing

---
+++ BankManager/BankManager/Dummies/StatusDummyRepository.cs
@@ -0,0 +1,31 @@
+﻿using BankManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankManager.Dummies
+{
+    public class StatusDummyRepository : IRepository<ClientStatus>
+    {
+        public int Create(ClientStatus t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(ClientStatus t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ClientStatus> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientStatus FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
